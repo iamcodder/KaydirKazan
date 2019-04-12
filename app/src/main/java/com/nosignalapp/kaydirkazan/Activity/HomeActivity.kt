b27d30df441227660b79eb879be907a8116fcddb
Süleyman Sezer <me.iamcodder@gmail.com>
2019-04-12 23:56:00 +0300

    iamcodder
    gradient

--- app/src/main/java/com/nosignalapp/kaydirkazan/Activity/HomeActivity.kt
+++ app/src/main/java/com/nosignalapp/kaydirkazan/Activity/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.nosignalapp.kaydirkazan.Activity
 
 import android.app.ActivityManager
 import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -42,6 +43,10 @@ class HomeActivity : AppCompatActivity() ,HomeContract.View {
         mUser= FirebaseAuth.getInstance().currentUser!!
         mKullanici=userModel()
         iintent= Intent(this, SoruActivity::class.java)
+        var animation_drawable:AnimationDrawable= activity_home_constraint.background as AnimationDrawable
+        animation_drawable.setEnterFadeDuration(1000)
+        animation_drawable.setExitFadeDuration(2000)
+        animation_drawable.start()
     }
 
     override fun clickControl() {
